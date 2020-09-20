d0bc82a0f9e49e54f1b58aa5eeab3adea413c2e0
Avishith <pmshithu143@gmail.com>
2020-09-20 12:07:41 +0530

    work started for my first node.js application

---
+++ app.js
@@ -0,0 +1,56 @@
+// if (process.env.NODE_ENV !=="production"){
+//   require("dotenv").load()
+  
+// }
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const expressLayouts= require("express-ejs-layouts")
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set("layout","layouts/layout")
+app.use(expressLayouts)
+
+const mongoose =require('mongoose');
+const { error } = require('console');
+// mongoose.connect(process.env.DATABASE_URL,{useNewUrlPaser:true})
+// const db =mongoose.connection
+// db.on('error',error => console.error(error))
+// db.once('open',() => console.log('connected '))
+ 
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
